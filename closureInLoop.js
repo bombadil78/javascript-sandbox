60cc24ffca9a0b136a7e58891ec8b5aab3d7dae1
christoph <chkeller@gmx.net>
2016-09-14 14:53:37 +0200

    Added closure in loop

---
+++ closureInLoop.js
@@ -0,0 +1,38 @@
+function emitVariable(i) {
+    console.log("variable=" + i);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var list = document.getElementById("my-list");
+    var staticValue = 123;
+    var variableValue = 1;
+    let otherVariableValue = 1;
+
+    var currentChild = list.firstElementChild;
+    while (currentChild !== null) {
+        currentChild.addEventListener('click', function() {
+            console.log("staticValue" + staticValue);
+            console.log("variableValue=" + variableValue);
+            console.log("is bound at evaluation time of this closure (!)");
+            console.log("otherVariableValue=" + otherVariableValue);
+        });
+        currentChild = currentChild.nextElementSibling;
+        variableValue++;
+        otherVariableValue++;
+    }
+
+    var otherList = document.getElementById("my-other-list");
+    var v = 0;
+
+    var currentChild = otherList.firstElementChild;
+    while (currentChild !== null) {
+        currentChild.onclick = function(v) {
+            var handler = function() {
+                console.log(v);
+            };
+            return handler;
+        }(++v); // bind to parameter AND evaluate function inside the loop (!!!)
+        currentChild = currentChild.nextElementSibling;
+    }
+});
--- patterns.js
+++ patterns.js
@@ -1,6 +1,5 @@
 var animal = function(id) {
-    var that = { id: id };
-    return that;
+    return {};
 };
 
 var bird = function(id, name) {
@@ -9,16 +8,18 @@ var bird = function(id, name) {
     // since it is the method form, this is bound to the object
     // the object was created by animal() and augmented here
     that.feep = function() {
-        return "Feep: " + this.id + " / " + this.name;
+        return "Feep: " + id + " / " + this.name;
     };
     return that;
 };
 
 var superBird = function(id, name) {
     var that = bird(id, name);
-    var normalFeep = that.feep();
+    // get a reference to super method ...
+    var superFeep = that.feep;
     that.feep = function() {
-        return normalFeep + "!!!";
+        // ... apply it on subclasses' object
+        return superFeep.apply(that);
     };
     return that;
 };
